be3d7bc62770d2ec90101174b86672334f4f6e1d
Lorenzo D'Alessandro <lo.dalessandro@reply.it>
2022-06-13 17:24:37 +0200

    added danger file

---
+++ Dangerfile.swift
@@ -0,0 +1,32 @@
+import Danger
+
+let danger = Danger()
+
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+let createdFiles = danger.git.createdFiles
+let modifiedFiles = danger.git.modifiedFiles
+
+if !createdFiles.isEmpty {
+    print("Added files:")
+    createdFiles.forEach {
+        message("- \($0.name)")
+    }
+}
+
+if !modifiedFiles.isEmpty {
+    print("Edited files:")
+    modifiedFiles.forEach {
+        print("- \($0.name)")
+    }
+}
+
+let forceUnwrapRegex = "([A-z]+[A-Za-z0-9]*![.,:)\n\t\r ])"
+
+allSourceFiles.forEach {
+    let result = danger.utils.readFile($0).range(of: forceUnwrapRegex,
+                                                 options: .regularExpression) != nil
+    if result {
+        warn("Found force unwrapped optional value")
+    }
+}
+
