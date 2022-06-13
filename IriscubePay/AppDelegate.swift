068637da81d1218a024d3a9322a20f7bc025bad9
Lorenzo D'Alessandro <lo.dalessandro@reply.it>
2022-06-13 17:35:30 +0200

    push for testing actions

--- IriscubePay/AppDelegate.swift
+++ IriscubePay/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var appCoordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         guard let window = window else { return false }
         
